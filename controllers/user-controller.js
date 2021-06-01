bd8a1d97c954a9781b6557eab0867534204617f1
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-01 13:44:23 -0700

    get all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    // get all Users
+    getUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select:'-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    
+}
