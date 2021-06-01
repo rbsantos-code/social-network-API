83cfe446f2ea3387594c16a6841b87e2a42b6369
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-01 14:00:01 -0700

    delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -47,5 +47,40 @@ const userController = {
         User.create(body)
         .then(userData => res.json(userData))
         .catch(err => res.status(400).json(err));
+    },
+
+    // update User
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true})
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    // delete User
+    deleteUser({ params}, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     }
-}
+};
+
+
+module.exports = userController;
